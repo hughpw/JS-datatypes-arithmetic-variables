b048d10dd575fbfb71880e6e84dfb20aafe63011
Hugh Kwabiah <hughkwabiah@gmail.com>
2022-06-17 14:37:11 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+
+// console.log("'ello 'ello 'ello");
+
+// alert("What's up?");
+
+// document.write("Hi");
+
+// prompt("type something");
+
+// Numbers & Arithmetic;
+
+// console.log(10 * 10);
+
+// console.log(18 % 3);
+
+// console.log(5 ** 2);
+
+// let iCanChange = "James";
+
+// const iCannotChange = "Jonathan";
+
+// console.log(iCanChange);
+
+// console.log(iCannotChange);
+
+// iCannotChange = "Paul"; Error message returned
+
+// iCanChange = "Aubrey";
+
+// console.log(iCanChange)
+
+// const firstNumber = 28;
+
+// const secondNumber = 23;
+
+// console.log(firstNumber + secondNumber)
+// console.log(iCanChange + " thinks he's a bicycle.")
+// console.log(secondNumber + " words&letters " + iCannotChange)
+
+// console.log("12" + "45")
+
+// let novelNumber = 89;
+// novelNumber *= 3
+// console.log(novelNumber)
+
+// let result = 12;
+
+// console.log(++result)
+
+// let course = 1;
+
+// if (course == 1) {
+//   document.write("<h1>HTML Tutorial</h1>");
+// }
+// else if (course == 2) {
+//   document.write("<h1>CSS Tutorial</h1>");
+// }
+// else {
+//   document.write("<h1>JavaScript Tutorial</h1>");
+// }
